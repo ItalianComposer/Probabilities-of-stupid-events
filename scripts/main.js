5de888d5592a08f35587d5541d6e3217bcd91e5c
ItalianComposer <movchan2001@bk.ru>
2022-07-09 12:06:08 +0300

    Версия 1.1.0 Добавлена возможность ввода пользовательских событий

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,28 @@ function calcProb() {
   objectVal = document.prob.object.value;
   placeVal = document.prob.place.value;
   let resultProb = actionVal * objectVal * placeVal;
-  console.log(resultProb);
+  // console.log(resultProb);
   document.getElementById("res").innerHTML = "Вероятность этого события " + resultProb + "%";
+};
+// function validate_form() {
+// 	valid = true;
+//   if ((document.probCustom.actionCustom.value == "") && (document.probCustom.objectCustom.value == "") && (document.probCustom.placeCustom.value == "")) {
+//     alert ("Пожалуйста заполните поля");
+//     valid = false;
+//   } return valid;
+// };
+function calcCustom() {
+  let actionValCustom = "";
+  let objectValCustom = "";
+  let placeValCustom = "";
+  actionValCustom = document.probCustom.actionCustom.value;
+  objectValCustom = document.probCustom.objectCustom.value;
+  placeValCustom = document.probCustom.placeCustom.value;
+  let primePercentile = Math.random() * (1 - 0.00000001) + 0.00000001;
+  let secondaryPercentile = primePercentile / 10000;
+  let resultProbCustom = "Вероятность события " + actionValCustom + " " + objectValCustom + " " + placeValCustom + " " + secondaryPercentile + "%";
+  console.log(resultProbCustom);
+  document.getElementById("resCustom").innerHTML = "Вероятность события " + actionValCustom + " " + objectValCustom + " " + placeValCustom + " " + secondaryPercentile + "%";
+  
+
 }
